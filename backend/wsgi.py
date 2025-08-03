25d3a23d52632bc91a1bb51070d0b05f49cb6719
ardawgcl3 <150267246+aryanhgit@users.noreply.github.com>
2025-08-03 12:21:44 +0530

    Add JWT token to wsgi.py

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -7,23 +7,35 @@ from models import Posts, User
 from extension import db
 from posts import post_ns
 from auth import auth_ns
+from flask_cors import CORS
+from places import place_ns
+from optimize import optimize_ns
+import os
 
-# Create Flask app and load config
+
 app = Flask(__name__)
+CORS(app)
 app.config.from_object(DevConfig)
 
 # Initialize extensions
 db.init_app(app)
 
 migrate = Migrate(app, db)
+app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
 jwt = JWTManager(app)
 
 # Set up Flask-RESTX API 
 api = Api(app) 
 api.add_namespace(post_ns)
 api.add_namespace(auth_ns)
+api.add_namespace(place_ns)
+api.add_namespace(optimize_ns)
 
 # Shell context for Flask CLI
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'Posts': Posts, 'User': User}
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
